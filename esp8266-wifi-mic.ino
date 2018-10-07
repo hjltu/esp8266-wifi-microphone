ada8613930b1d4378a94d5a65e7f21ba1744da59
hjltu <hjltu@ya.ru>
2018-10-07 21:52:28 +0500

    change button and led pins

--- esp8266-wifi-mic.ino
+++ esp8266-wifi-mic.ino
@@ -1,26 +1,41 @@
+// Project name: hjhome
+// Copyright (C) 2016  hjltu@ya.ru
+// https://launchpad.net/hjhome
+
+// hjhome is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// hjhome is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	//	FOR esp-07	//
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-const char *ssid =  "ssid";
-const char *pass =  "pass";
+const char *ssid =  "hj-dir-300";
+const char *pass =  "9090068900";
 
 WiFiClient wclient;
-
-// mqtt server
 IPAddress mqtt_server(192,168,0,20);
 PubSubClient client(wclient, mqtt_server);
 
 //	client name
-String str_name="esp";
+String str_name="esp1";
 
-String str_topic = String("/" + str_name + "/out/rec");
+String str_topic = String("/" + str_name + "/out/stream");
 String str_payload;
 
 bool bt;
 byte rec;
 int count;
-//char stream[24000];
 
 void setup()
 {
@@ -28,8 +43,9 @@ void setup()
 	Serial.println(0);			//start
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(ssid, pass);
-	pinMode(0,INPUT_PULLUP);	//	button D3
-	pinMode(16,OUTPUT);			//	led D0
+	pinMode(4,INPUT_PULLUP);	//	bt
+	pinMode(2,OUTPUT);			//	led
+	delay(3000);
 }
 
 void loop()
@@ -53,8 +69,8 @@ void my_connect()
 		if(WiFi.waitForConnectResult() != WL_CONNECTED)
 		{
 			Serial.println(2);		//WiFi NOT connected
-			if(digitalRead(10)==LOW)
-				digitalWrite(10,HIGH);
+			if(digitalRead(2)==LOW)
+				digitalWrite(2,HIGH);
 			return;
 		}
 		Serial.println(3);			//WiFi connected
@@ -67,40 +83,57 @@ void my_connect()
 			if(client.connect(str_name))
 			{
 				Serial.println(5);				//Connected to MQTT server
+				client.set_callback(callback);
+				client.subscribe("/" + str_name + "/#");
 			}
 			else
 			{
 				Serial.println(6);		//Could not connect to MQTT server
-				if(digitalRead(10)==LOW)
-					digitalWrite(10,HIGH);
+				if(digitalRead(2)==LOW)
+					digitalWrite(2,HIGH);
 			}
 		}
 		if(client.connected())
 		{
 			client.loop();
-			if(digitalRead(10)==HIGH)
-				digitalWrite(10,LOW);
+			if(digitalRead(2)==HIGH)
+				digitalWrite(2,LOW);
+		}
+	}
+}
+
+void callback(const MQTT::Publish& pub)
+{
+	String inc_topic = pub.topic();
+	String inc_payload = pub.payload_string();
+	
+	if(inc_topic.indexOf("/in")>0)
+	{
+		Serial.println(F("incoming"));
+		if(inc_topic.indexOf("/led")>0)
+		{
+			if(inc_payload=="0" || inc_payload=="false" || inc_payload=="OFF")
+				if(bt==false)
+					bt=true;
 		}
 	}
 }
 
 void my_print()
 {
-	//Serial.println(millis());
 	client.publish(str_topic,str_payload);
 	str_payload="";
-	//Serial.println(millis());
 }
 
 void my_button()
 {
-	if(digitalRead(12)==LOW && count<15)
+	if(digitalRead(4)==LOW && count<15)
 		count++;
-	if(digitalRead(12)==HIGH && count>0)
+	if(digitalRead(4)==HIGH && count>0)
 		count--;
 	if(count>10 && bt==false)
 		bt=true;
-	if(count<5 && bt==true)
+	if(count==0 && bt==true)
 		bt=false;
 }
 
@@ -108,19 +141,23 @@ void my_analog()
 {
 	if(bt==true)
 	{
-		//Serial.println(millis());
+		if(digitalRead(2)==LOW)
+			digitalWrite(2,HIGH);
 		my_record();
-		//Serial.println(millis());
 	}
 }
 
 void my_record()
 {
 	bt=false;
-	for(int y=0;y<11;y++)
+	for(int y=0;y<31;y++)
 	{
 		for(int i=0;i<1000;i++)
+		{
 			str_payload += char(analogRead(A0)/4);
-		my_print();
+		}
+	my_print();
 	}
+	if(digitalRead(2)==HIGH)
+		digitalWrite(2,LOW);
 }
