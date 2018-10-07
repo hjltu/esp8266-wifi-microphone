71a2b872dc0a628a86a447230fc81a4dc766d134
hjltu <hjltu@ya.ru>
2018-10-07 22:01:25 +0500

    Update esp8266-wifi-mic.ino

--- esp8266-wifi-mic.ino
+++ esp8266-wifi-mic.ino
@@ -1,41 +1,24 @@
-// Project name: hjhome
-// Copyright (C) 2016  hjltu@ya.ru
-// https://launchpad.net/hjhome
-
-// hjhome is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// hjhome is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-	//	FOR esp-07	//
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-const char *ssid =  "hj-dir-300";
-const char *pass =  "9090068900";
+const char *ssid =  "SSID";
+const char *pass =  "PASS";
 
 WiFiClient wclient;
-IPAddress mqtt_server(192,168,0,20);
+IPAddress mqtt_server(192,168,0,10);
 PubSubClient client(wclient, mqtt_server);
 
 //	client name
-String str_name="esp1";
+String str_name="nodemcu";
 
-String str_topic = String("/" + str_name + "/out/stream");
+String str_topic = String("/" + str_name + "/out/rec");
 String str_payload;
 
 bool bt;
 byte rec;
 int count;
+//char stream[24000];
 
 void setup()
 {
@@ -43,9 +26,8 @@ void setup()
 	Serial.println(0);			//start
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(ssid, pass);
-	pinMode(4,INPUT_PULLUP);	//	bt
-	pinMode(2,OUTPUT);			//	led
-	delay(3000);
+	pinMode(0,INPUT_PULLUP);	//	bt D3
+	pinMode(16,OUTPUT);			//	rele D0
 }
 
 void loop()
@@ -69,8 +51,8 @@ void my_connect()
 		if(WiFi.waitForConnectResult() != WL_CONNECTED)
 		{
 			Serial.println(2);		//WiFi NOT connected
-			if(digitalRead(2)==LOW)
-				digitalWrite(2,HIGH);
+			if(digitalRead(10)==LOW)
+				digitalWrite(10,HIGH);
 			return;
 		}
 		Serial.println(3);			//WiFi connected
@@ -83,57 +65,40 @@ void my_connect()
 			if(client.connect(str_name))
 			{
 				Serial.println(5);				//Connected to MQTT server
-				client.set_callback(callback);
-				client.subscribe("/" + str_name + "/#");
 			}
 			else
 			{
 				Serial.println(6);		//Could not connect to MQTT server
-				if(digitalRead(2)==LOW)
-					digitalWrite(2,HIGH);
+				if(digitalRead(16)==LOW)
+					digitalWrite(16,HIGH);
 			}
 		}
 		if(client.connected())
 		{
 			client.loop();
-			if(digitalRead(2)==HIGH)
-				digitalWrite(2,LOW);
-		}
-	}
-}
-
-void callback(const MQTT::Publish& pub)
-{
-	String inc_topic = pub.topic();
-	String inc_payload = pub.payload_string();
-	
-	if(inc_topic.indexOf("/in")>0)
-	{
-		Serial.println(F("incoming"));
-		if(inc_topic.indexOf("/led")>0)
-		{
-			if(inc_payload=="0" || inc_payload=="false" || inc_payload=="OFF")
-				if(bt==false)
-					bt=true;
+			if(digitalRead(16)==HIGH)
+				digitalWrite(16,LOW);
 		}
 	}
 }
 
 void my_print()
 {
+	//Serial.println(millis());
 	client.publish(str_topic,str_payload);
 	str_payload="";
+	//Serial.println(millis());
 }
 
 void my_button()
 {
-	if(digitalRead(4)==LOW && count<15)
+	if(digitalRead(0)==LOW && count<15)
 		count++;
-	if(digitalRead(4)==HIGH && count>0)
+	if(digitalRead(0)==HIGH && count>0)
 		count--;
 	if(count>10 && bt==false)
 		bt=true;
-	if(count==0 && bt==true)
+	if(count<5 && bt==true)
 		bt=false;
 }
 
@@ -141,23 +106,25 @@ void my_analog()
 {
 	if(bt==true)
 	{
-		if(digitalRead(2)==LOW)
-			digitalWrite(2,HIGH);
+		//Serial.println(millis());
 		my_record();
+		//Serial.println(millis());
 	}
 }
 
 void my_record()
 {
 	bt=false;
-	for(int y=0;y<31;y++)
+	for(int y=0;y<11;y++)
 	{
 		for(int i=0;i<1000;i++)
 		{
+			//rec = char(analogRead(A0)/4);
+			//if(rec<200)
+				//str_payload += rec;
 			str_payload += char(analogRead(A0)/4);
+			//delayMicroseconds(25);	//125-8kb
 		}
 	my_print();
 	}
-	if(digitalRead(2)==HIGH)
-		digitalWrite(2,LOW);
 }
